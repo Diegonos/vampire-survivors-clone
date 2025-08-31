a77f2fe67886cebdae91c758cfad17322b5810aa
Diego Nosari <diego_nos@hotmail.it>
2025-08-31 18:55:41 +0200

    Added tornado attack

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,15 +3,25 @@ extends CharacterBody2D
 var movement_speed = 40.0
 var hp = 80
 
+var last_movement = Vector2.UP
+
 var iceSpear = preload("res://scenes/ice_spear.tscn")
+var tornado = preload("res://scenes/tornado.tscn")
 
 @onready var iceSpearTimer = get_node("%IceSpearTimer")
 @onready var iceSpearAttackTimer = get_node("%IceSpearAttackTimer")
+@onready var tornadoTimer = get_node("%TornadoTimer")
+@onready var tornadoAttackTimer = get_node("%TornadoAttackTimer")
 
 var iceSpear_ammo = 0
 var iceSpear_baseAmmo = 1
 var iceSpear_attackSpeed = 1.5
-var iceSpear_level = 1
+var iceSpear_level = 0
+
+var tornado_ammo = 0
+var tornado_baseAmmo = 1
+var tornado_attackSpeed = 3
+var tornado_level = 1
 
 var enemy_close = []
 
@@ -26,7 +36,10 @@ func attack():
 		iceSpearTimer.wait_time = iceSpear_attackSpeed
 		if iceSpearTimer.is_stopped():
 			iceSpearTimer.start()
-		
+	if tornado_level > 0:
+		tornadoTimer.wait_time = tornado_attackSpeed
+		if tornadoTimer.is_stopped():
+			tornadoTimer.start()
 
 func _physics_process(_delta: float) -> void: 
 	movement()
@@ -41,6 +54,7 @@ func movement():
 	elif mov.x < 0:
 		sprite.flip_h = false
 	if mov != Vector2.ZERO:
+		last_movement = mov
 		if walk_timer.is_stopped():
 			if sprite.frame >= sprite.hframes - 1:
 				sprite.frame = 0
@@ -72,6 +86,24 @@ func _on_ice_spear_attack_timer_timeout() -> void:
 			iceSpearAttackTimer.start()
 		else:
 			iceSpearAttackTimer.stop()
+			
+func _on_tornado_timer_timeout() -> void:
+	tornado_ammo += tornado_baseAmmo
+	tornadoAttackTimer.start()
+
+
+func _on_tornado_attack_timer_timeout() -> void:
+	if tornado_ammo > 0:
+		var tornado_attack = tornado.instantiate()
+		tornado_attack.position = position
+		tornado_attack.last_movement = last_movement
+		tornado_attack.level = tornado_level
+		add_child(tornado_attack)
+		tornado_ammo -= 1
+		if tornado_ammo > 0:
+			tornadoAttackTimer.start()
+		else:
+			tornadoAttackTimer.stop()
 		
 
 func get_random_target():
