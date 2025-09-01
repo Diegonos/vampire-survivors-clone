ff90dada833db38c6493bed9886be681a59da70c
Diego Nosari <diego_nos@hotmail.it>
2025-09-01 21:03:36 +0200

    Added javelin attack

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,11 +7,13 @@ var last_movement = Vector2.UP
 
 var iceSpear = preload("res://scenes/ice_spear.tscn")
 var tornado = preload("res://scenes/tornado.tscn")
+var javelin = preload("res://scenes/javelin.tscn")
 
 @onready var iceSpearTimer = get_node("%IceSpearTimer")
 @onready var iceSpearAttackTimer = get_node("%IceSpearAttackTimer")
 @onready var tornadoTimer = get_node("%TornadoTimer")
 @onready var tornadoAttackTimer = get_node("%TornadoAttackTimer")
+@onready var javelin_base: Node = get_node("%JavelinBase")
 
 var iceSpear_ammo = 0
 var iceSpear_baseAmmo = 1
@@ -21,7 +23,10 @@ var iceSpear_level = 0
 var tornado_ammo = 0
 var tornado_baseAmmo = 1
 var tornado_attackSpeed = 3
-var tornado_level = 1
+var tornado_level = 0
+
+var javelin_ammo = 1
+var javelin_level = 1
 
 var enemy_close = []
 
@@ -40,6 +45,8 @@ func attack():
 		tornadoTimer.wait_time = tornado_attackSpeed
 		if tornadoTimer.is_stopped():
 			tornadoTimer.start()
+	if javelin_level > 0:
+		spawn_javelin()
 
 func _physics_process(_delta: float) -> void: 
 	movement()
@@ -104,7 +111,15 @@ func _on_tornado_attack_timer_timeout() -> void:
 			tornadoAttackTimer.start()
 		else:
 			tornadoAttackTimer.stop()
-		
+			
+func spawn_javelin():
+	var get_javelin_total = javelin_base.get_child_count()
+	var calc_spawns = javelin_ammo - get_javelin_total
+	while calc_spawns > 0:
+		var javelin_spawn = javelin.instantiate()
+		javelin_spawn.global_position = global_position
+		javelin_base.add_child(javelin_spawn)
+		calc_spawns -= 1
 
 func get_random_target():
 	if enemy_close.size() > 0:
